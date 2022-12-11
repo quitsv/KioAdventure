943f56ae7285dc977112ffb061abaae140232d59
Renald Kharisma Tjandra <nyxargentum16@gmail.com>
2022-12-12 00:01:08 +0700

    add change Weapon Feature

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,50 +2,101 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-  [SerializeField] private float attackCooldown;
-  private Animator anim;
-  private PlayerMovement playerMovement;
-  private float cooldownTimer = Mathf.Infinity;
-  [SerializeField] private Transform attackPoint;
-  [SerializeField] private LayerMask enemyLayers;
-  [SerializeField] private int attackDamage = 40;
-  private float attackRange = 1;
+    [SerializeField] private float attackCooldown;
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    private float cooldownTimer = Mathf.Infinity;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private LayerMask enemyLayers;
+    [SerializeField] private int attackDamage = 40;
+    [SerializeField] private int spearAttackDamage = 60;
+    private float attackRange = 1;
 
-  private void Awake()
-  {
-    anim = GetComponent<Animator>();
-    playerMovement = GetComponent<PlayerMovement>();
-  }
+    int currentWeaponNumber;
 
-  private void Update()
-  {
-    if (Input.GetMouseButton(0) && cooldownTimer >= attackCooldown && playerMovement.canAttack())
+    private void Awake()
     {
-      Attack();
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
     }
-    cooldownTimer += Time.deltaTime;
-  }
 
-  private void Attack()
-  {
-    if (PauseMenu.isPaused())
-      return;
-
-    anim.SetTrigger("attack");
-    cooldownTimer = 0;
-    Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
-
-    foreach (Collider2D enemy in hitEnemies)
+    private void Update()
     {
-      enemy.GetComponent<Health>().TakeDamage(attackDamage);
-    }
-  }
+        if (Input.GetMouseButton(0) && cooldownTimer >= attackCooldown && playerMovement.canAttack())
+        {
+            if (currentWeaponNumber == 0)
+            {
+                Attack();
+            }
+            else
+            {
+                SpearAttack();
+            }
+        }
 
-  private void OnDrawGizmosSelected()
-  {
-    if (attackPoint == null)
-      return;
-    //menampilkan range attack
-    Gizmos.DrawWireSphere(attackPoint.position, attackRange);
-  }
+        cooldownTimer += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ChangeWeapon();
+        }
+
+    }
+
+
+    void ChangeWeapon()
+    {
+        if (currentWeaponNumber == 0)
+        {
+            currentWeaponNumber += 1;
+            anim.SetLayerWeight(currentWeaponNumber - 1, 0);
+            anim.SetLayerWeight(currentWeaponNumber, 1);
+        }
+        else
+        {
+            currentWeaponNumber -= 1;
+            anim.SetLayerWeight(currentWeaponNumber + 1, 0);
+            anim.SetLayerWeight(currentWeaponNumber, 1);
+        }
+    }
+
+    private void Attack()
+    {
+        if (PauseMenu.isPaused())
+            return;
+
+        anim.SetTrigger("attack");
+        cooldownTimer = 0;
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Health>().TakeDamage(attackDamage);
+        }
+
+    }
+
+    private void SpearAttack()
+    {
+        if (PauseMenu.isPaused())
+            return;
+
+        anim.SetTrigger("SpearAttack");
+        cooldownTimer = 0;
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Health>().TakeDamage(spearAttackDamage);
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        //menampilkan range attack
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
 }

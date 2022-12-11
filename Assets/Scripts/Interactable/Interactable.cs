9896d4e0230d6a9900355ffdb680e5578559b87e
Sean <patriksean@ymail.com>
2022-12-12 00:02:18 +0700

    nambahMusicdanBuatPuzzle

---
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode intractKey;
+    public UnityEvent interactAction;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
